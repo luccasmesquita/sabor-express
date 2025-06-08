f7dd734eef6948a8b0631171eb926dd0dd884b65
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-06-08 12:17:21 +0100

    tratando erro de tipos de dados

--- app.py
+++ app.py
@@ -15,21 +15,31 @@ def finalizar_app():
     # os.system('cls') / no windowns
     print('Finalizando o app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+def opcao_invalida():
+    print('Opção invalida!\n')
+    input('Pressione "enter" para voltar ao menu principal!')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else :
-        print('Escolha uma opção valida')
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else :
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
